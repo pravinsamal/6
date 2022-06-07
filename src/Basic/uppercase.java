dd3e8aa50c66c653b00506f786d7260e45361331
pravinsamal <samalp911@gmail.com>
2022-06-08 00:04:35 +0530

    lin

--- src/Basic/uppercase.java
+++ src/Basic/uppercase.java
@@ -4,7 +4,7 @@ public class uppercase {
 	public static void main(String args[]){
 		String s1="hello string";
 		String s1upper=s1.toUpperCase();
-		String s2="PRAVIN SAMAL";
+		String s2="PRAVIN   LINGRAJ SAMAL";
 		System.out.println(s2.toLowerCase());
 		System.out.println(s1upper);
 		}
