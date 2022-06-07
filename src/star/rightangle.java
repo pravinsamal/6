393e66693bc7ab22643650ec8c239067d5445952
pravinsamal <samalp911@gmail.com>
2022-06-07 23:59:05 +0530

    done adding

--- src/star/rightangle.java
+++ src/star/rightangle.java
@@ -2,14 +2,15 @@ package star;
 
 public class rightangle {
 	public static void main(String[] args) {
-		for(int a=1; a<=15; a++) {
+		for(int a=1; a<=10; a++) {
 			for(int c=1; c<=5; c++) {
 				System.out.print("  ");
 			}
 			for(int b=1; b<=15-a; b++) {
 				System.out.print("*");
 			}
-			System.out.println(" ");
+			System.out.println(" ");///dscvasdvcdsvsd
+			// do the  changes and add it in git
 		}
 	}
 }
